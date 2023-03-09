e0af9865c1de1dd4a1633a16ff9ff051eb34d933
Jérémie Guy <guyjeremix@gmail.com>
2023-03-09 19:12:56 +0100

    first woking version
    2D + cylinder

--- lb.py
+++ lb.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # selection of site
 # Advection of incoming particles of direct neighborhood
@@ -7,15 +8,24 @@ import matplotlib.pyplot as plt
 # post-collision rearrangement
 # next site selection
 
+ # plot
+plottingFrames = 50
 
+# animation
+fig, ax = plt.subplots()
+ims = []
+
+# distance function : used to compute cylinder diameter
+def distance(x1,y1,x2,y2):
+    return np.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 def main():
     ''' Variables '''
     # lattice dimensions = nb cells
-    Nx = 300 
+    Nx = 400
     Ny = 100 # for 2D
-    # Timesteps (simulation duration)
-    Nt = 1000
+    # Timesteps (simulation duration = iterations)
+    Nt = 3000
     # time scale = kinematic velocity = relaxation time
     tau = .53
 
@@ -25,23 +35,104 @@ def main():
 
     '''Lattice Speeds & Weights''' # each direction has a set speed of 1 to go to a node in any direction
     # Nb of velocities (= neighbours)
-    NL = 3 # 1D
-    # NL = 9 # 2D
+    # NL = 3 # 1D
+    NL = 9 # 2D
 
     # discreete velocities of each neihbouring node
-    cx = np.array([0,-1,1])  # 1D
-    # 2D
-    # cx = np.array([0,-1,0,1,1, 1, 0,-1,-1])
-    # cy = np.array([0, 1,1,1,0,-1,-1,-1, 0])
+    # 1D - nodes order [center, left, right]
+    # cxs = np.array([0,-1,1])  
+    # 2D - nodes order : [center, top left, top center, top right, medium center, low right, low center, left center, medium left]
+    cxs = np.array([0,-1,0,1,1, 1, 0,-1,-1])
+    cys = np.array([0, 1,1,1,0,-1,-1,-1, 0])
     
     # weights
-    weights = np.array([2/9,5/9,2/9]) # 1D
-    # weights = np.array([4/9,1/36,1/9,1/36,1/9,1/36,1/9,1/36,1/9]) # 2D
+    # weights = np.array([2/9,5/9,2/9]) # 1D
+    weights = np.array([4/9,1/36,1/9,1/36,1/9,1/36,1/9,1/36,1/9]) # 2D
 
     '''Initial conditions'''
-    # "phase space " = distribution function of 3 dimensions : 1 or2 geometrical dimension(s), 1 velocity dimension
-    f = np.ones([Nx,NL]) # 1D
-    # f = np.ones([Nx,Ny,NL]) # 2D
+    # "phase space " = distribution function of 3 dimensions : 1 or2 geometrical dimension(s), 1 velocity dimension (= mesoscopic velocity)
+    # use of np.ones because more stable than np.zeroes (source : dude trust me)
+    # f = np.ones([Nx//deltaX,NL]) # 1D
+    # f = np.ones([Nx//deltaX,NL]) + .01 * np.random.randn(Nx, NL) # 1D + randomness (adds 'swirly' effect to fluid)
+    # f = np.ones([Ny//deltaX,Nx//deltaX,NL]) # 2D
+    f = np.ones((Ny//deltaX,Nx//deltaX,NL)) + .01 * np.random.randn(Ny, Nx, NL) # 2D
 
+    # Initial directionnal velocity (to the right in this case)
+    # f[:,2] = 2.3 # 1D
+    f[:,:, 4] = 2.3 # 2D
+
+    '''Optionnal : obstacle (2D)'''
+    # define cylinder as array (same size as phase space) with initial False value 
+    cylinder = np.full((Ny, Nx), False) 
+    # if point (x,y) is in cylinder, set value to True
+    for y in range(0,Ny):
+        for x in range(0,Nx):
+            if (distance(Nx//4,Ny//2,x,y)<10): # set center at half of y height and 1/4 of x distance ('//' = integer division)
+                cylinder[y][x] = True
+    
+    '''Main Loop'''
+    for it in range(Nt//deltaT):
+        print("iteration : ", it)
+
+        ''' Streaming step '''
+        # for  i, cx in zip(range(NL),cxs): # 1D, zip = (it,cx,cy), total = NL
+        #     f[:,i] = np.roll(f[:,i],cx,axis = 1)
+
+        for  i, cx, cy in zip(range(NL),cxs,cys): # 2D, zip = (it,cx,cy), total = NL
+            f[:,:,i] = np.roll(f[:,:,i],cx,axis = 1)
+            f[:,:,i] = np.roll(f[:,:,i],cy,axis = 0)
+
+        # set boundaries
+        boundaryF = f[cylinder,:] # set the nodes that are in the cylinder ... 
+        boundaryF = boundaryF[:,[0,5,6,7,8,1,2,3,4]] # ... and bounce them back = define their velocity as the one in the opposite direction
+
+        ''' Fluid variables '''
+
+        # defining density to compute equilibrium set (1D)
+        # rho = np.sum(f, 1)
+        # momentum
+        # ux = np.sum(f * cxs, 1) / rho
+
+        # defining density to compute equilibrium set (2D)
+        rho = np.sum(f, 2)
+        # momentum
+        ux = np.sum(f * cxs, 2) / rho
+        uy = np.sum(f * cys, 2) / rho
+
+        # Boundary step (cylinder - 2D only)
+        f[cylinder,:] = boundaryF
+        ux[cylinder] = 0
+        uy[cylinder] = 0
+
+        ''' collision step '''
+        feq = np.ones(f.shape) # initialize equilibrium state
+        # compute equilibrium state : 
+        # for i , cx, w in zip((range(NL), cxs, weights)): # 1D
+        #     feq[:, i] = w * rho * (1 + 3 * (cx*ux) + 9 * (cx*ux)**2 / 2 - 3  * (ux**2) / 2)
+        for i, cx,cy,w in zip(range(NL),cxs,cys,weights): # 2D
+            feq[:,:,i] = w * rho * (1 + 3 * (cx*ux + cy*uy) + 9 * (cx*ux + cy*uy)**2 / 2 - 3  * (ux**2 + uy**2) / 2)
+
+        # compute next step
+        f = f + -(1/tau) * (f - feq)
+
+        ''' Plot '''
+        if (it%plottingFrames == 0):
+            # plt.imshow(np.sqrt(ux**2)) # 1D
+            plt.imshow(np.sqrt(ux**2+uy**2)) # 2D
+            plt.pause(.01)
+            plt.cla()
+
+        # ''' Animation '''
+        # # im = ax.plot(np.sqrt(ux**2+uy**2)) # F*ucked up but nice
+        # im = ax.imshow(np.sqrt(ux**2+uy**2))
+        # ims.append(im)
+
+
+
+# def animate(i):
+#     return ims[i]
 
 main()
+
+# ani = animation.ArtistAnimation(fig, animate, interval=50, blit=True, repeat_delay=1000)
+# plt.show()
--- testing.py
+++ testing.py
@@ -12,6 +12,51 @@ def testMeshGrid():
     plt.plot(X,Y, 'o')
     plt.show()
 
+def testMatrix():
+    x = np.linspace(0,300)
+    
+    y = np.linspace(0,100,100)
+    # print(y)
+    # plt.plot(x,y, 'o')
+    # plt.show()
+    print(np.ones((30,10)))
+
+def testFull():
+    x = np.full((10,2),False)
+    print(x)
+
+def testDivide():
+    x = 100
+    print(x/4)
+    print(int(x/4))
+    print(x//4)
+
+def testRange():
+    for i in range(0,14):
+        print(i)
+    
+def rolling():
+    x =  [  [[ 0, 1,  2,  3], 
+            [ 4,  5,  6,  7],
+            [ 8,  9, 10, 11]],
+            
+            [[ 0, 1,  2,  3], 
+            [ 4,  5,  6,  7],
+            [ 8,  9, 10, 11]]]
+    
+def testSum():
+    x = np.ones([10,5,3])
+    b = [0,-1,1]
+    print(x)
+    print(sum(x,2))
+    print(sum(x*b,2))
 
 
-testMeshGrid()
+
+# testMatrix()
+# testMeshGrid()
+# testFull()
+# testDivide()
+# testRange()
+# rolling()
+testSum()
